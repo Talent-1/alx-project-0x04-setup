4bfa85342aad4e15036140237b8ccf2b7a8aad20
Talent-1 <itzmhiztatalent@gmail.com>
2025-11-30 21:12:32 +0100

    project-0x05

--- alx-project-0x05/pages/_app.tsx
+++ alx-project-0x05/pages/_app.tsx
@@ -1,4 +1,4 @@
-import Layout from "
+import Layout from "@/components/layouts/Layout";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { CountProvider } from "@context/CountContext";
